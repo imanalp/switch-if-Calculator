ba2d019c7885b2333f8f9f75d98b221dc90bc86c
imanalp <imanalp@DESKTOP-A3FHE66>
2025-12-04 03:24:34 +0330

    Add project files.

---
+++ my first calculator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_first_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number 1:  ");
+            double firstnumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Number 2:  ");
+            double secondnumber = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter opeator (+, _, *, /) : ");
+            string operation = Console.ReadLine();
+
+            double result;
+
+            switch (operation)
+            {
+                case "+":
+                    result = firstnumber + secondnumber;
+                    Console.WriteLine($"Result: {result}");
+                    break;
+                case "-":
+                    result = firstnumber - secondnumber;
+                    Console.WriteLine($"Result: {result}");
+                    break;
+                case "*":
+                    result = firstnumber * secondnumber;
+                    Console.WriteLine($"Result: {result}");
+                    break;
+                case "/":
+                    if (secondnumber == 0)
+                    {
+                        Console.WriteLine("Error: cannot divide by zero.");
+                    }
+                    else
+                    {
+                        result = firstnumber / secondnumber;
+                        Console.WriteLine($"Result: {result}");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("invalid operator. please enter +, _, * or /.");
+                    break;
+            }
+
+
+
+
+        }
+    }
+}
---
+++ my first calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("my first calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("my first calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7b00a4e4-ed5f-4623-9b99-4495e25343ac")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
